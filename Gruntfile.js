d96cb35a3cb6074bc670f5a7ce1c81037230d45b
shrikarch <shrikar.code@gmail.com>
2017-03-27 12:54:06 -0500

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+        less: {
+            development: {
+                options: {
+                    paths: ["less/"]
+                },
+                files: {"css/style.css": "less/style.less"}
+            },
+            production: {
+                options: {
+                    paths: ["less/"],
+                    cleancss: true
+                },
+                files: {"css/style.css": "less/style.less"}
+            }
+        },
+        cssmin: {
+            dist: {
+                files: {'css/style.min.css':'css/style.css'}
+            }
+        }, 
+        watch: {
+            dist: {
+                files: ['less/*.less','js/scripts.js'],
+                tasks: ['less','cssmin']
+            },
+            grunt: {
+                files: ['Gruntfile.js']
+            },
+            options:{
+                livereload: true
+            }
+        }});
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['watch']);
+    //grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-less');
+};
