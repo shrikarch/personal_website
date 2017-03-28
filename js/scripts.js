ba2ef78f5108ac714f2c6909cd8c480024431c2a
shrikarch <shrikar.code@gmail.com>
2017-03-28 15:46:37 -0500

    styling changes - minor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
+function animateBars(){
+    $('.progress-bar').each(function(){
+        $(this).animate({
+            width: $(this).attr("aria-valuenow") + "%"
+        });
+    });
+};
+
+
 $(document).ready(function(){
-   
-});
+
+    $('#skills_trigger').waypoint(function(direction){
+        if(direction == 'down'){
+            animateBars();
+        };
+    });
+
+
+
+
+}); //domready ends
+
+
