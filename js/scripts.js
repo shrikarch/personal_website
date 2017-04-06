4de12a03e03b9d92784bc879c16415a647069b56
shrikarch <shrikar.code@gmail.com>
2017-04-06 17:16:52 -0500

    Added responsiveness

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,34 @@ function animateBars(){
     });
 };
 
+function addIf(screenspec){
+    if(screenspec == 'mobile'){
+        $('#firstname_sidebar').append('<span id="skills_trigger" class="hidden-sm-up"></span>');
+        $('.excess_on_sm').slideUp();
+    }else if(screenspec == 'desktop'){
+        $('#elixir_github').append('<span id="skills_trigger" class="hidden-sm-up"></span>');
+    }
+};
 
 $(document).ready(function(){
 
-    $('#skills_trigger').waypoint(function(direction){
-        if(direction == 'down'){
-            animateBars();
-        };
+    var screenSpecs = function(){
+        return $('html').attr('class').split(" ")
+    }
+    console.log(screenSpecs()[1]);
+
+    addIf(screenSpecs()[1]);
+    $('.show_more_text').text(
+        "Show " + $('.right_section .excess_on_sm .info_block').length+" more"
+    );
+    $('.show_more_block .btn').click(function(){
+        $('.excess_on_sm').slideDown("slow");
+        $('.show_more_block').fadeOut();
+        
     });
 
+
+    //Large Screens exclusive
     $('#showProjects').click(function(){
         $(this).addClass('active_top_link'); 
         $('#showResume').removeClass('active_top_link');
@@ -28,6 +47,15 @@ $(document).ready(function(){
         $('.all_resume').slideDown();
         $('.all_projects').slideUp()
     });
+
+
+    //universal
+    $('#skills_trigger').waypoint(function(direction){
+        if(direction == 'down'){
+            animateBars();
+        };
+    });
+    console.log($('.right_section .excess_on_sm .info_block').length);
 
     //$('.all_resume').slideUp();
     //$('.all_projects').slideDown();
