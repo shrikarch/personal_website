3663ea0cb767d2c89e84065c0afd014bd0d8f8ab
shrikarch <shrikar.code@gmail.com>
2017-04-05 22:26:54 -0500

    minor changes and prep for portfolio addition

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,23 @@ $(document).ready(function(){
         };
     });
 
+    $('#showProjects').click(function(){
+        $(this).addClass('active_top_link'); 
+        $('#showResume').removeClass('active_top_link');
+        $('.all_resume').slideUp();
+        $('.all_projects').slideDown();
+    });
+
+    $('#showResume').click(function(){
+        $(this).addClass('active_top_link'); 
+        $('#showProjects').removeClass('active_top_link');
+        $('.all_resume').slideDown();
+        $('.all_projects').slideUp()
+    });
+
+    //$('.all_resume').slideUp();
+    //$('.all_projects').slideDown();
+
 
 
 
