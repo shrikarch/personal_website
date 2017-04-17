4a007a5b42cf2805b010e644a3f3271697fbb29a
shrikarch <shrikar.code@gmail.com>
2017-04-16 22:53:33 -0500

    Initial projects in portfolio section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,10 @@ import { SectionSwitcherComponent } from './section-switcher/section-switcher.co
 const appRoutes: Routes = [
   { path: '', component: ResumeComponent},
   { path: 'resume', component: ResumeComponent},
-  { path: 'portfolio', component: PortfolioComponent}
+  { path: 'portfolio', component: PortfolioComponent, children: [
+    { path: 'circleframe', component: CircleframeComponent },
+    { path: 'sharpenerinc', component: SharpenerincComponent }
+  ]}
 ]
 
 @NgModule({
