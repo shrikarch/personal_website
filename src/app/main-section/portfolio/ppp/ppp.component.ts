ab9c524710afff85ad52edb0b1ec924c74d21b5d
shrikarch <shrikar.code@gmail.com>
2017-10-10 21:14:12 -0400

    added ppp info. fixed broken links. added site links.

--- src/app/main-section/portfolio/ppp/ppp.component.ts
+++ src/app/main-section/portfolio/ppp/ppp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Image } from '../images.model';
 
 @Component({
   selector: 'app-ppp',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ppp.component.less']
 })
 export class PppComponent implements OnInit {
+
+
+  images: Image[] = [
+    new Image('assets/images/ppp/j360_mobile_footer.png','png','lorem'),
+    new Image('assets/images/ppp/ppp_desk_footer.png','png','lorem'),
+    new Image('assets/images/ppp/ppp_desk_landing.png','png','lorem'),
+    new Image('assets/images/ppp/ppp_mobile_bio.png','png','lorem'),
+    new Image('assets/images/ppp/ppp_mobile_bioEx.png','png','lorem'),
+    new Image('assets/images/ppp/ppp_mobile_landing.png','png','lorem'),
+    new Image('assets/images/ppp/ppp_FullSite.jpg','jpg','lorem')
+  ]
+
+  mockImages: Image[] =[];
 
   constructor() { }
 
--- src/app/sidebar/skills-section/skills-section.component.ts
+++ src/app/sidebar/skills-section/skills-section.component.ts
@@ -8,11 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class SkillsSectionComponent implements OnInit {
 skills = [
   {"name":"HTML & CSS", "value":95},
-  {"name":"JavaScript", "value":85},
+  {"name":"JavaScript", "value":90},
+  {"name":"NodeJs", "value":85},
   {"name":"Elixir", "value":75},
-  {"name":"Angular2", "value":75},
+  {"name":"REST APIs / ExpressJS", "value":85},
   {"name":"AngularJs", "value":80},
-  {"name":"NodeJs", "value":55},
+  {"name":"Angular2", "value":68},
   {"name":"PHP", "value":60},
   {"name":"ReactJs", "value":20},
 ]
